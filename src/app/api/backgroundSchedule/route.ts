d68c7e06b46219660a92bd7ba56e6805ebd81aa3
Menglay <cheng.menglay79@gmail.com>
2025-01-11 15:32:43 +0700

    Fix error

--- src/app/api/backgroundSchedule/route.ts
+++ src/app/api/backgroundSchedule/route.ts
@@ -4,6 +4,7 @@ import dayjs from "dayjs";
 export async function GET() {
   try {
     const currentDate = dayjs().toDate();
+
     // Archive outdated schedules with booked seats
     await prisma.schedule.updateMany({
       where: {
@@ -21,36 +22,42 @@ export async function GET() {
       },
     });
 
-    // Prepare new schedules without IDs and with updated data
+    // Prepare new schedules
     const newSchedules = archivedSchedules.map(({ ...schedule }) => ({
-      ...schedule,
+      busId: schedule.busId,
+      routeId: schedule.routeId,
       departure_date: null, // Update with a valid default if required
+      departure_time: schedule.departure_time,
+      arrival_time: schedule.arrival_time,
+      boarding_point: schedule.boarding_point,
+      boarding_url: schedule.boarding_url,
+      dropping_point: schedule.dropping_point,
+      dropping_url: schedule.dropping_url,
+      price: schedule.price,
+      available_seat: schedule.available_seat,
       status: "Active",
     }));
 
     // Batch create new schedules
     if (newSchedules.length > 0) {
-      newSchedules.map((schedule) =>
-        prisma.schedule.create({
-          data: {
-            busId: schedule.busId,
-            routeId: schedule.routeId,
-            departure_date: null,
-            departure_time: schedule.departure_time,
-            arrival_time: schedule.arrival_time,
-            boarding_point: schedule.boarding_point,
-            boarding_url: schedule.boarding_url,
-            dropping_point: schedule.dropping_point,
-            dropping_url: schedule.dropping_url,
-            price: schedule.price,
-            available_seat: schedule.available_seat,
-          },
-        })
+      await Promise.all(
+        newSchedules.map((schedule) =>
+          prisma.schedule.create({ data: schedule })
+        )
       );
     }
 
-    console.log(`Archived and created ${newSchedules.length} new schedules.`);
+    return new Response(
+      JSON.stringify({
+        message: `Archived and created ${newSchedules.length} new schedules.`,
+      }),
+      { status: 200, headers: { "Content-Type": "application/json" } }
+    );
   } catch (error) {
     console.error("Error during the cron job:", error);
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
   }
 }
