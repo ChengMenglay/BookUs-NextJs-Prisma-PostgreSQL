55bdc67562386776c895ed83ed3f745e0d0d1fdf
Menglay <cheng.menglay79@gmail.com>
2025-01-13 11:16:31 +0700

    Fix PassengerForm Issue

--- src/app/(Main)/transport/[origin]/[destination]/[departure_date]/[scheduleId]/components/PassengerForm.tsx
+++ src/app/(Main)/transport/[origin]/[destination]/[departure_date]/[scheduleId]/components/PassengerForm.tsx
@@ -92,7 +92,7 @@ export default function PassengerForm({
     try {
       setIsLoading(true);
       const result = await axios.post(
-        `${process.env.FRONTEND_URL}/api/checkout`,
+        `/api/checkout`,
         {
           scheduleId,
           userId: currentUser?.id,
